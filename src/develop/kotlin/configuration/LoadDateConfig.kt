5f82fb32de0f230bcd5dfd865807aa5b32080e77
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2019-12-30 08:49:39 +0100

    load authorities test user

--- src/develop/kotlin/configuration/LoadDateConfig.kt
+++ src/develop/kotlin/configuration/LoadDateConfig.kt
@@ -3,6 +3,7 @@ package community.flock.eco.fundraising.configuration
 import community.flock.eco.feature.member.data.MemberLoadData
 import community.flock.eco.feature.payment.data.PaymentLoadData
 import community.flock.eco.fundraising.data.ExcelLoadData
+import community.flock.eco.fundraising.data.UserLoadData
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
@@ -12,5 +13,6 @@ import org.springframework.context.annotation.Import
 @Import(
         MemberLoadData::class,
         PaymentLoadData::class,
-        ExcelLoadData::class)
+        ExcelLoadData::class,
+        UserLoadData::class)
 class LoadDateConfig
