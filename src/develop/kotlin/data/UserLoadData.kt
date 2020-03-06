90c428be4763c832fc27a3ec74d3d1512d83f365
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2020-03-06 10:20:49 +0100

    chore: bump to flock-eco 1.6.6

--- src/develop/kotlin/data/UserLoadData.kt
+++ src/develop/kotlin/data/UserLoadData.kt
@@ -5,6 +5,7 @@ import community.flock.eco.feature.member.data.MemberLoadData
 import community.flock.eco.feature.member.repositories.MemberRepository
 import community.flock.eco.feature.payment.data.PaymentLoadData
 import community.flock.eco.feature.user.forms.UserAccountPasswordForm
+import community.flock.eco.feature.user.model.User
 import community.flock.eco.feature.user.model.UserAccount
 import community.flock.eco.feature.user.repositories.UserRepository
 import community.flock.eco.feature.user.services.UserAccountService
@@ -25,22 +26,24 @@ import javax.annotation.PostConstruct
         "flock.fundraising.load-data.enabled",
         "flock.fundraising.load-data.stub")
 class UserLoadData(
+        private val userService: UserService,
         private val userAccountService: UserAccountService,
         private val userAuthorityService: UserAuthorityService
 ){
 
-    val data:MutableMap<String, UserAccount> = mutableMapOf()
+    val data:MutableMap<String, User> = mutableMapOf()
 
     @PostConstruct
     fun init() {
 
-        data["test"] = userAccountService.createUserAccountPassword(UserAccountPasswordForm(
+        data["test"] = userService.findByEmail("test") ?: userAccountService
+                .createUserAccountPassword(UserAccountPasswordForm(
                email = "test",
                password = "test",
                authorities = userAuthorityService.allAuthorities()
                        .map { it.toName() }
                        .toSet()
-       ))
+       )).user
 
     }
 
